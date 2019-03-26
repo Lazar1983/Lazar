822aabba087c5475270a84ab6dfeef15180d77ab
Lazar1983 <47781390+Lazar1983@users.noreply.github.com>
2019-03-26 12:30:46 +0100

    Delete games.js

--- games.js
+++
@@ -1,9 +0,0 @@
-function Game () {
-    this.registeredGames = [];
-    this.name ="Game day";
-
-    this.registerGame = function(target) {
-        this.registeredGames.push(target);
-    }
-    
-}
