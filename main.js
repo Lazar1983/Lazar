879b7abaac72313660f5e2ce6af7e89362f1af72
Lazar1983 <47781390+Lazar1983@users.noreply.github.com>
2019-03-26 12:31:14 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function main() {
-    var footballClub = new FootballClub("Chelsea");
-    var stadium = new Stadium ("Stanford Bridge");
-    console.log(footballClub);
-    console.log(stadium);
-}
-
-main();
